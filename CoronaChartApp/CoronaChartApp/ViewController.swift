af340411bb0ffb32c1e51522da100e67868f9772
you junyong <hellgey777@naver.com>
2022-03-07 15:03:36 +0900

    init

---
+++ CoronaChartApp/CoronaChartApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoronaChartApp
+//
+//  Created by 유준용 on 2022/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
